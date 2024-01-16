9ca01beec98c27c300042a0a567662126e112239
sidvmahesh <siddharth.mahesh@ucsf.edu>
2024-01-16 11:51:31 -0800

    initial commit with filled in functions

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,20 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    rna = []
+    for i in range(len(seq)):
+        rna.append(seq[i] if seq[i] != "T" else "U")
+    
+    return "".join(rna)
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    
+    rna = []
+    for i in range(len(seq)):
+        rna.append(seq[i] if seq[i] != "T" else "U")
+    
+    return "".join(rna)[::-1]
--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -25,11 +25,17 @@ def test_transcribe():
     """
     Write your unit test for the transcribe function here.
     """
-    pass
+    assert transcribe("") == ""
+    assert transcribe("AAAAACAAG") == "AAAAACAAG"
+    assert transcribe("TTTT") == "UUUU"
+    assert transcribe("ACGTCAGT") == "ACGUCAGU"
 
 
 def test_reverse_transcribe():
     """
     Write your unit test for the reverse transcribe function here.
     """
-    pass
+    assert reverse_transcribe("") == ""
+    assert reverse_transcribe("AAAAACAAG") == "GAACAAAAA"
+    assert reverse_transcribe("TTTT") == "UUUU"
+    assert reverse_transcribe("ACGTCAGT") == "UGACUGCA"
